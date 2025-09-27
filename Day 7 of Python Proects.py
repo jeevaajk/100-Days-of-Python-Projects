6ff278f882ef7ba14a26076afc7114743e284d35
Jeevaa JK <jeevaajk2007@gmail.com>
2025-09-27 23:34:48 +0530

    Add files via upload

---
+++ Day 7 of Python Proects.py
@@ -0,0 +1,25 @@
+# Simple Calculator in Python
+
+def calculator():
+    print("Simple Calculator")
+    print("Operations: +  -  *  /")
+    
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if op == "+":
+        print("Result:", num1 + num2)
+    elif op == "-":
+        print("Result:", num1 - num2)
+    elif op == "*":
+        print("Result:", num1 * num2)
+    elif op == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero not allowed!")
+    else:
+        print("Invalid operator!")
+
+calculator()
