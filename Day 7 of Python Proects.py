1ba210122cc1c9fb3171a866dfd37256ae93bd0f
Jeevaa JK <jeevaajk2007@gmail.com>
2025-10-02 22:12:23 +0530

    Day 7 of Python Proects.py

--- Day 7 of Python Proects.py
+++ Day 7 of Python Proects.py
@@ -1,5 +1,10 @@
 # Simple Calculator in Python
-
+print('''    ____    _    _     ____ _   _ _        _  _____ ___  ____  
+  / ___|  / \  | |   / ___| | | | |      / \|_   _/ _ \|  _ \ 
+ | |     / _ \ | |  | |   | | | | |     / _ \ | || | | | |_) |
+ | |___ / ___ \| |__| |___| |_| | |___ / ___ \| || |_| |  _ < 
+  \____/_/   \_\_____\____|\___/|_____/_/   \_\_| \___/|_| \_\
+                                                              ''')
 def calculator():
     print("Simple Calculator")
     print("Operations: +  -  *  /")
@@ -23,3 +28,4 @@ def calculator():
         print("Invalid operator!")
 
 calculator()
+
